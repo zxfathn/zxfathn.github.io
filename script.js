24abdf7a811ca7965d0ab23598f7a8a96fdacda3
zxfathn <jikaj4903@gmail.com>
2025-11-09 17:06:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-ini script.js const API_URL = "https://script.google.com/macros/s/AKfycbydEpfOgZhBuKqdoROmXlIYi41PW9E5YpECmUhu-Mrhgaku1Pchf3KVqbZ9bkiJa7rvNw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbydEpfOgZhBuKqdoROmXlIYi41PW9E5YpECmUhu-Mrhgaku1Pchf3KVqbZ9bkiJa7rvNw/exec";
 
 const contactsTable = document.getElementById("contactsTable");
 const contactForm = document.getElementById("contactForm");
